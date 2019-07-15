f8381d4d7923955d85e3ec5e213c9d0866a396c9
claudio <cmartinsgyn@gmail.com>
2019-07-14 23:36:33 -0300

    layout menu cadastros

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -13,9 +13,9 @@ export const ROUTES: RouteInfo[] = [
     { path: '/usuario-list', title: 'Lista de Usuários',  icon: 'list', class: '' },
     { path: '/boletim-list', title: 'Lista Boletins',  icon: 'list', class: '' },
     { path: '/boletim-cadastro', title: 'Cadastro de Boletim',  icon: 'library_books', class: '' },
-    { path: '/typography', title: 'Typography',  icon: 'library_books', class: '' },
-    { path: '/icons', title: 'Icons',  icon: 'bubble_chart', class: '' },
-    { path: '/notifications', title: 'Notifications',  icon: 'notifications', class: '' },
+    // { path: '/typography', title: 'Typography',  icon: 'library_books', class: '' },
+    // { path: '/icons', title: 'Icons',  icon: 'bubble_chart', class: '' },
+    // { path: '/notifications', title: 'Notifications',  icon: 'notifications', class: '' },
 ];
 
 @Component({
